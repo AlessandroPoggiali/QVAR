2c909a3f69c27dd6b62e620d729c88592ccb7bc1
alessandroPoggiali <alessandro.poggiali97@gmail.com>
2023-11-28 16:11:00 +0100

    first commit

---
+++ QVAR.py
@@ -0,0 +1,113 @@
+from qiskit import ClassicalRegister, QuantumRegister, QuantumCircuit, execute, BasicAer
+from qiskit.algorithms import EstimationProblem
+from qiskit.algorithms import AmplitudeEstimation
+from qiskit.primitives import Sampler
+
+
+# U: state preparation
+# var_index: list of qubit indices of which we want to compute the variance
+# version: method for estimating the variance
+#
+#    'FAE'    (default) Faster Amplitude Estimation
+#    'AE'     Amplitude Estimation
+#    'SHOTS'  measurements with multiple circuit execution
+#
+# delta (optional)                : target accuracy (FAE)
+# max_iter (optional)             : maximum number of iterations of the oracle (FAE)
+# eval_qbits (optional)           : number of additional qubits (AE)
+# shots (optional)                : number of shots (SHOTS)
+# normalization_factor (optional) : multiplicative constant to obtain the final value
+def QVAR(U, var_index=None, version='FAE', delta=0.0001, max_iter=5, eval_qbits=5, shots=8192, normalization_factor=None):
+
+    if var_index == None:
+        var_index = [x for x in range(U.num_qubits)]
+    
+    i_qbits = len(var_index)
+    q_qbits = e_qbits = i_qbits
+    u_qbits = U.num_qubits
+
+    a = QuantumRegister(1,'a')
+    e = QuantumRegister(e_qbits,'e')
+    q = QuantumRegister(q_qbits,'q')
+    u = QuantumRegister(u_qbits, 'u')
+
+    if version == 'SHOTS':
+        ca = ClassicalRegister(1,'ca')
+        cq = ClassicalRegister(q_qbits,'cq')
+        ce = ClassicalRegister(e_qbits,'ce')
+        qc = QuantumCircuit(a, e, q, u, ca, cq, ce)
+    
+    else:
+        qc = QuantumCircuit(a, e, q, u)
+
+    qc.append(U.to_gate(), [qc.num_qubits - i - 1 for i in range(u_qbits)]) 
+    
+    qc.h(a)
+    qc.cx(a,e)
+    qc.x(e)
+
+    for t in range(i_qbits):
+        qc.cswap(a,q[t],u[var_index[t]])
+
+    for t in range(i_qbits):
+        qc.ch(a,u[var_index[t]])
+        
+    qc.ch(a,e)
+    qc.h(a)
+
+    qc.h(q)
+
+    qc.x(q)
+    
+    if version == 'SHOTS':
+        qc.measure(a, ca) 
+        qc.measure(q, cq)
+        qc.measure(e, ce)   
+
+        backend = BasicAer.get_backend('qasm_simulator')
+        counts = execute(qc, backend, shots=shots).result().get_counts(qc)
+        target_conf = '1'*e_qbits + ' ' + '1'*q_qbits + " 1"
+
+        try: 
+            var = (counts[target_conf])/shots
+        except:
+            var = 0
+            
+    elif version == 'AE':
+        sampler = Sampler()
+        ae = AmplitudeEstimation(
+            num_eval_qubits=eval_qbits,  # the number of evaluation qubits specifies circuit width and accuracy
+            sampler=sampler
+        )
+
+        problem = EstimationProblem(
+            state_preparation=qc,  # A operator
+            objective_qubits=[x for x in range(qc.num_qubits-i_qbits)],
+        )
+        ae_result = ae.estimate(problem)
+
+        var = ae_result.mle
+        
+    elif version == 'FAE':
+        sampler = Sampler()
+        fae = FasterAmplitudeEstimation(
+            delta=delta,  # target accuracy
+            maxiter=max_iter,  # determines the maximal power of the Grover operator
+            sampler=sampler,
+        )
+
+        problem = EstimationProblem(
+            state_preparation=qc,  # A operator
+            objective_qubits=[x for x in range(qc.num_qubits-i_qbits)],
+        )
+        fae_result = fae.estimate(problem)
+        var = fae_result.estimation
+
+    if normalization_factor is not None:
+        var = var*normalization_factor
+    else:    
+        n_hadamard = 2+i_qbits
+        norm_factor = 2**n_hadamard/2**i_qbits
+        var = var*norm_factor
+
+    return var
---
+++ test.py
@@ -0,0 +1,41 @@
+import numpy as np
+from qiskit import QuantumCircuit, execute, Aer
+from QVAR import QVAR
+
+def get_statevector(circuit):
+    backend = Aer.get_backend('statevector_simulator')
+    job = execute(circuit, backend=backend, shots=1024, memory=True)
+    job_result = job.result()
+    statevector = job_result.get_statevector(circuit)
+    tol = 1e-16
+    statevector = np.asarray(statevector)
+    statevector.real[abs(statevector.real) < tol] = 0.0
+    statevector.imag[abs(statevector.imag) < tol] = 0.0
+    return statevector.real
+
+if __name__ == "__main__":
+
+    u_size = 2
+
+    c_variances = []
+    q_variances = []
+
+    for t in range(5):
+        U = QuantumCircuit(u_size)
+        U.h([x for x in range(u_size)])
+        
+        for z in range(u_size):
+            U.ry(np.random.uniform(0, 2*np.pi), z)
+        
+        c_var = np.var(get_statevector(U))
+        q_var = QVAR(U, version='FAE')
+        
+        print(str(c_var)+ ' - ' + str(q_var))
+        
+        c_variances.append(c_var)
+        q_variances.append(q_var)
+
+
+    differences = [(q-c)**2 for q,c in zip(q_variances, c_variances)]
+
+    print("MSE: " + str(np.mean(differences)))
