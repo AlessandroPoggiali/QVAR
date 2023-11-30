b413967639a3d732fde98264b39fd465b62a4563
alessandroPoggiali <alessandro.poggiali97@gmail.com>
2023-11-30 19:57:43 +0100

    new test

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import numpy as np
-from qiskit import QuantumCircuit, execute, Aer
+import math
+from qiskit import QuantumCircuit, execute, Aer, QuantumRegister
 from qvar import QVAR
+
 
 def get_statevector(circuit):
     backend = Aer.get_backend('statevector_simulator')
@@ -13,14 +15,20 @@ def get_statevector(circuit):
     statevector.imag[abs(statevector.imag) < tol] = 0.0
     return statevector.real
 
-if __name__ == "__main__":
+def _register_switcher(circuit, value, qubit_index):
+    bin_str_pattern = '{:0%sb}' % len(qubit_index)
+    value = bin_str_pattern.format(value)[::-1]
+    for idx, bit in enumerate(value):
+        if not int(bit):
+            circuit.x(qubit_index[idx])
 
-    u_size = 2
+def test_random_U(u_size):
 
     c_variances = []
     q_variances = []
 
-    for t in range(5):
+    print("Classical - Quantum")
+    for _ in range(5):
         U = QuantumCircuit(u_size)
         U.h([x for x in range(u_size)])
         
@@ -39,3 +47,45 @@ if __name__ == "__main__":
     differences = [(q-c)**2 for q,c in zip(q_variances, c_variances)]
 
     print("MSE: " + str(np.mean(differences)))
+
+def test_ffqram(N):
+    cl = []
+    qu = []
+
+    print("Classical - Quantum")
+    for _ in range(5):
+        vector = np.random.uniform(-1,1, N)
+        n = math.ceil(math.log2(N))
+
+        r = QuantumRegister(1, 'r') 
+        i = QuantumRegister(n, 'i')  
+
+        U = QuantumCircuit(i, r)
+
+        U.h(i)
+
+        for index, val in zip(range(len(vector)), vector):
+            _register_switcher(U, index, i)
+            U.mcry(np.arcsin(val)*2, i[0:], r) 
+            _register_switcher(U, index, i)
+            
+        q_var = QVAR(U, var_index=list(range(n)), ps_index=[U.num_qubits-1], version='FAE', n_h_gates=n)
+        classical = np.var(vector)
+        print(str(classical)+ " - " + str(q_var))
+        qu.append(q_var)
+        cl.append(classical)
+
+    differences = [(q-c)**2 for q,c in zip(qu, cl)]
+    print("MSE: " + str(np.mean(differences)))
+
+
+if __name__ == "__main__":
+
+    
+    
+
+    print("\n RANDOM UNITARY TEST \n")
+    test_random_U(2)
+
+    print("\n FF-QRAM TEST \n")
+    test_ffqram(8)
