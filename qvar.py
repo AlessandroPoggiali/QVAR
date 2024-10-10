753e9d9dda50f62a68d80429531e84e9f5157b46
alessandroPoggiali <alessandro.poggiali97@gmail.com>
2024-10-10 16:49:02 +0200

    noise added

--- qvar.py
+++ qvar.py
@@ -1,7 +1,20 @@
-from qiskit import ClassicalRegister, QuantumRegister, QuantumCircuit, execute, BasicAer
-from qiskit.algorithms import EstimationProblem
-from qiskit.algorithms import AmplitudeEstimation, FasterAmplitudeEstimation
+from qiskit import *
+from qiskit.circuit.library import *
+from qiskit.circuit import *
+from qiskit.quantum_info import *
+from qiskit.circuit.library import UnitaryGate
+
+import numpy as np
+import math
+
+from qiskit.quantum_info import *
+import matplotlib.pyplot as plt
+
+from qiskit_algorithms import EstimationProblem
+from qiskit_algorithms import AmplitudeEstimation, FasterAmplitudeEstimation
 from qiskit.primitives import Sampler
+
+from qiskit.transpiler import CouplingMap, Layout
 
 # QVAR subroutine for computing the variance of a set of values encoded into a quantum state
 #
@@ -22,8 +35,136 @@ from qiskit.primitives import Sampler
 # shots (optional)          : number of shots (SHOTS)
 # n_h_gates (optional)      : normalization constant to multiply the final value
 # postprocessing (optional) : if True, return the MLE postprocessed value (only for AE)
+# backend (optional):       : specified backend on which the simulation will be run
 
-def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter=5, eval_qbits=5, shots=8192, n_h_gates=0, postprocessing=True):
+def QVAR_old(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter=5, eval_qbits=5, shots=8192, n_h_gates=0, postprocessing=True, backend=None):
+
+    if var_index is None:
+        var_index = [x for x in range(U.num_qubits)]
+    
+    i_qbits = len(var_index)
+    q_qbits = e_qbits = i_qbits
+    u_qbits = U.num_qubits
+
+    a = QuantumRegister(1,'a')
+    e = QuantumRegister(e_qbits,'e')
+    q = QuantumRegister(q_qbits,'q')
+    u = QuantumRegister(u_qbits, 'u')
+
+    if version == 'SHOTS':
+        ca = ClassicalRegister(1,'ca')
+        cq = ClassicalRegister(q_qbits,'cq')
+        ce = ClassicalRegister(e_qbits,'ce')
+        if ps_index is not None:
+            cps = ClassicalRegister(len(ps_index), 'cps')
+            qc = QuantumCircuit(a, e, q, u, ca, cq, ce, cps)
+        else:
+            qc = QuantumCircuit(a, e, q, u, ca, cq, ce)
+    
+    else:
+        qc = QuantumCircuit(a, e, q, u)
+
+    qc.append(U.to_gate(), list(range(1+e_qbits+q_qbits, qc.num_qubits)))    
+    
+    qc.h(a)
+    qc.cx(a,e)
+    qc.x(e)
+
+    for t in range(i_qbits):
+        qc.cswap(a,q[t],u[var_index[t]])
+
+    for t in range(i_qbits):
+        qc.ch(a,u[var_index[t]])
+        
+    qc.ch(a,e)
+    qc.h(a)
+
+    qc.h(q)
+
+    qc.x(q)
+    
+    if ps_index is None:
+        objective_qubits = [x for x in range(1+e_qbits+q_qbits)]
+    else:
+        objective_qubits = [x for x in range(1+e_qbits+q_qbits)]+[qc.num_qubits-u_qbits + x for x in ps_index]
+    
+    if version == 'SHOTS':
+        qc.measure(a, ca) 
+        qc.measure(q, cq)
+        qc.measure(e, ce)
+        
+        if ps_index is not None:
+            qc.measure(u[ps_index], cps)
+            target_conf = '1'*len(ps_index) + ' ' + '1'*e_qbits + ' ' + '1'*q_qbits + ' 1' 
+        else:
+            target_conf = '1'*e_qbits + ' ' + '1'*q_qbits + ' 1'
+
+        counts = backend.run(transpile(qc, backend), shots=shots).result().get_counts()
+
+        try: 
+            var = (counts[target_conf])/shots
+        except:
+            var = 0
+            
+    elif version == 'AE':
+        sampler = Sampler()
+        sampler.set_options(backend=backend)
+        ae = AmplitudeEstimation(
+            num_eval_qubits=eval_qbits,  
+            sampler=sampler,
+        )
+        
+        problem = EstimationProblem(
+            state_preparation=qc, 
+            objective_qubits=objective_qubits,
+        )
+        ae_result = ae.estimate(problem)
+
+        if postprocessing:
+            var = ae_result.mle
+        else:
+            var = ae_result.estimation
+        
+    elif version == 'FAE':
+        sampler = Sampler()
+        sampler.set_options(backend=backend)
+        fae = FasterAmplitudeEstimation(
+            delta=delta, 
+            maxiter=max_iter,  
+            sampler=sampler
+        )
+
+        problem = EstimationProblem(
+            state_preparation=qc, 
+            objective_qubits=objective_qubits,
+        )
+        fae_result = fae.estimate(problem)
+        var = fae_result.estimation
+    
+    elif version == 'STATEVECTOR':
+        
+        problem = EstimationProblem(
+            state_preparation=qc, 
+            objective_qubits=objective_qubits,
+        )
+
+        transpiled_circuit = transpile(qc, backend, optimization_level=0, coupling_map=backend.coupling_map, seed_transpiler=123)
+        transpiled_circuit.save_statevector()
+        statevector = np.asarray(backend.run(transpiled_circuit, seed_simulator=123).result().get_statevector())
+        
+        var = 0
+        for i, amplitude in enumerate(statevector):
+            full_state = bin(i)[2:].zfill(qc.num_qubits)[::-1]
+            state = ''.join([full_state[i] for i in objective_qubits])
+            if problem.is_good_state(state[::-1]):
+                var = var + np.abs(amplitude) ** 2
+    
+    tot_hadamard = 2 + i_qbits + n_h_gates
+    norm_factor = 2**tot_hadamard/2**i_qbits
+
+    return var*norm_factor
+
+def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter=5, eval_qbits=5, shots=8192, n_h_gates=0, postprocessing=True, backend=None):
 
     if var_index is None:
         var_index = [x for x in range(U.num_qubits)]
@@ -47,7 +188,7 @@ def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter
     
     else:
         qc = QuantumCircuit(a, e, u)
-
+    
     qc.append(U.to_gate(), list(range(1+e_qbits, qc.num_qubits)))    
     
     qc.h(a)
@@ -76,9 +217,8 @@ def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter
             target_conf = '1'*len(ps_index) + ' ' + '1'*e_qbits + ' 1' 
         else:
             target_conf = '1'*e_qbits + ' 1'
-
-        backend = BasicAer.get_backend('qasm_simulator')
-        counts = execute(qc, backend, shots=shots).result().get_counts(qc)
+            
+        counts = backend.run(transpile(qc, backend), shots=shots).result().get_counts()
 
         try: 
             var = (counts[target_conf])/shots
@@ -87,6 +227,7 @@ def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter
             
     elif version == 'AE':
         sampler = Sampler()
+        sampler.set_options(backend=backend)
         ae = AmplitudeEstimation(
             num_eval_qubits=eval_qbits,  
             sampler=sampler
@@ -101,14 +242,15 @@ def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter
         if postprocessing:
             var = ae_result.mle
         else:
-            var = ae.estimation
+            var = ae_result.estimation
         
     elif version == 'FAE':
         sampler = Sampler()
+        sampler.set_options(backend=backend)
         fae = FasterAmplitudeEstimation(
             delta=delta, 
             maxiter=max_iter,  
-            sampler=sampler,
+            sampler=sampler
         )
 
         problem = EstimationProblem(
@@ -117,10 +259,26 @@ def QVAR(U, var_index=None, ps_index=None, version='FAE', delta=0.0001, max_iter
         )
         fae_result = fae.estimate(problem)
         var = fae_result.estimation
+
+    elif version == 'STATEVECTOR':
         
+        problem = EstimationProblem(
+            state_preparation=qc, 
+            objective_qubits=objective_qubits,
+        )
+
+        transpiled_circuit = transpile(qc, backend, optimization_level=0, coupling_map=backend.coupling_map, seed_transpiler=123)
+        transpiled_circuit.save_statevector()
+        statevector = np.asarray(backend.run(transpiled_circuit,seed_simulator=123).result().get_statevector())
+        
+        var = 0
+        for i, amplitude in enumerate(statevector):
+            full_state = bin(i)[2:].zfill(qc.num_qubits)[::-1]
+            state = ''.join([full_state[i] for i in objective_qubits])
+            if problem.is_good_state(state[::-1]):
+                var = var + np.abs(amplitude) ** 2
     
     tot_hadamard = 2 + n_h_gates
     norm_factor = 2**tot_hadamard/2**i_qbits
 
     return var*norm_factor
-
--- test.py
+++ test.py
@@ -1,19 +1,13 @@
 import numpy as np
 import math
-from qiskit import QuantumCircuit, execute, Aer, QuantumRegister
-from qvar import QVAR
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.providers.fake_provider import *
+from qiskit_aer.noise import NoiseModel
+from qiskit_aer import AerSimulator
+from qiskit.transpiler import CouplingMap
+from qvar import QVAR, QVAR_old
+import matplotlib.pyplot as plt
 
-
-def get_statevector(circuit):
-    backend = Aer.get_backend('statevector_simulator')
-    job = execute(circuit, backend=backend, shots=1024, memory=True)
-    job_result = job.result()
-    statevector = job_result.get_statevector(circuit)
-    tol = 1e-16
-    statevector = np.asarray(statevector)
-    statevector.real[abs(statevector.real) < tol] = 0.0
-    statevector.imag[abs(statevector.imag) < tol] = 0.0
-    return statevector.real
 
 def _register_switcher(circuit, value, qubit_index):
     bin_str_pattern = '{:0%sb}' % len(qubit_index)
@@ -27,6 +21,8 @@ def test_random_U(u_size):
     c_variances = []
     q_variances = []
 
+    backend = AerSimulator() # noiseless backend
+
     print("Classical - Quantum")
     for _ in range(5):
         U = QuantumCircuit(u_size)
@@ -35,8 +31,8 @@ def test_random_U(u_size):
         for z in range(u_size):
             U.ry(np.random.uniform(0, 2*np.pi), z)
         
-        c_var = np.var(get_statevector(U))
-        q_var = QVAR(U, version='FAE')
+        c_var = QVAR(U, version='STATEVECTOR', backend=backend)
+        q_var = QVAR(U, version='FAE', backend=backend)
         
         print(str(c_var)+ ' - ' + str(q_var))
         
@@ -51,6 +47,8 @@ def test_random_U(u_size):
 def test_ffqram(N):
     cl = []
     qu = []
+
+    backend = AerSimulator() # noiseless backend
 
     print("Classical - Quantum")
     for _ in range(5):
@@ -69,7 +67,7 @@ def test_ffqram(N):
             U.mcry(np.arcsin(val)*2, i[0:], r) 
             _register_switcher(U, index, i)
             
-        q_var = QVAR(U, var_index=list(range(n)), ps_index=[U.num_qubits-1], version='FAE', n_h_gates=n)
+        q_var = QVAR(U, var_index=list(range(n)), ps_index=[U.num_qubits-1], version='FAE', n_h_gates=n, backend=backend)
         classical = np.var(vector)
         print(str(classical)+ " - " + str(q_var))
         qu.append(q_var)
@@ -78,11 +76,78 @@ def test_ffqram(N):
     differences = [(q-c)**2 for q,c in zip(qu, cl)]
     print("MSE: " + str(np.mean(differences)))
 
+def test_noise():
+    '''
+    noisy_instance = Fake20QV1()
+    noise_model = NoiseModel.from_backend(noisy_instance)
+    coupling_map = noisy_instance.configuration().coupling_map
+    basis_gates = noise_model.basis_gates
+
+    backend = AerSimulator(noise_model=noise_model, coupling_map=coupling_map, basis_gates=basis_gates)
+    '''
+        
+    N_list = [2,4,8,16,32]
+    trial = 10
+
+    mae_qold = []
+    mae_qnew = []
+
+    for N in N_list:
+        print("N = " + str(N))
+        cl = []
+        q_old = []
+        q_new = []
+        
+        #print("Classical - QVAR_old - QVAR_new")
+        for i in range(trial):
+            np.random.seed(123*i)
+            vector = np.random.uniform(-1,1, N)
+            n = math.ceil(math.log2(N))
+        
+            r = QuantumRegister(1, 'r') 
+            i = QuantumRegister(n, 'i')  
+        
+            U = QuantumCircuit(i, r)
+            
+            U.h(i)
+            
+            for index, val in zip(range(len(vector)), vector):
+                _register_switcher(U, index, i)
+                U.mcry(np.arcsin(val)*2, i[0:], r) 
+                _register_switcher(U, index, i)
+            
+            n = int(np.log2(N)) 
+            backend = GenericBackendV2(3*n + 2)
+            q_var_old = QVAR_old(U, var_index=list(range(n)), ps_index=[U.num_qubits-1], version='STATEVECTOR', n_h_gates=n, backend=backend)
+            backend = GenericBackendV2(2*n + 2)
+            q_var_new = QVAR(U, var_index=list(range(n)), ps_index=[U.num_qubits-1], version='STATEVECTOR', n_h_gates=n, backend=backend)
+            classical = np.var(vector)
+            #print(str(classical)+ " - " + str(q_var_old) + " - " + str(q_var_new))
+            q_old.append(q_var_old)
+            q_new.append(q_var_new)
+            cl.append(classical)
+
+        mae_old = np.mean([abs(x-y) for x,y in zip(q_old, cl)])
+        mae_new = np.mean([abs(x-y) for x,y in zip(q_new, cl)])
+        print("MAE QVAR_old = " + str(mae_old))
+        print("MAE QVAR_new = " + str(mae_new))
+        mae_qold.append(mae_old)
+        mae_qnew.append(mae_new)
+
+    plt.scatter(N_list, mae_qold, label="MAE_QVAR_old")
+    plt.scatter(N_list, mae_qnew, label="MAE_QVAR_new")
+    plt.xticks(N_list)
+    plt.legend()
+    plt.savefig("results.png")
 
 if __name__ == "__main__":
 
+    '''
     print("\n RANDOM UNITARY TEST \n")
     test_random_U(2)
 
     print("\n FF-QRAM TEST \n")
     test_ffqram(8)
+    '''
+
+    test_noise()
